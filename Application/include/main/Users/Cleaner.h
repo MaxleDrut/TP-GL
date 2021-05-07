9a01aef96e84cbc0fc7f7bd7fa87121cdea66fbb
Maxence DRUTEL <49818023+MaxleDrut@users.noreply.github.com>
2021-05-07 15:04:12 +0200

    Create Cleaner.h

---
+++ Application/include/main/Users/Cleaner.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <time.h>
+
+class Cleaner {
+    public:
+
+        Cleaner(const double longi, const double lat, const time_t start, const time_t stop) : longitude(longi), latitude(lat), start(start), stop(stop) {
+        }
+
+        Cleaner() {
+            longitude = latitude = 0.0;
+            start = stop = NULL;
+        }
+
+        virtual ~Cleaner() {
+        }
+
+        virtual const time_t getStart() const{
+            return start;
+        } 
+
+        virtual const time_t getStop() const {
+            return stop;
+        }
+
+        virtual const double getLatitude() const {
+            return latitude;
+        }
+
+        virtual const double getLongitude() const {
+            return longitude;
+        }
+
+    private:
+        double latitude;
+        double longitude;
+        time_t start;
+        time_t stop;
+};
