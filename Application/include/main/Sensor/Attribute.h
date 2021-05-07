85124dd887de7b7d6ec6194d811cf4f91ada9236
Maxence DRUTEL <49818023+MaxleDrut@users.noreply.github.com>
2021-05-07 15:11:53 +0200

    Create Attribute.h

---
+++ Application/include/main/Sensor/Attribute.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <String>
+using namespace std;
+
+class Attribute {
+    public:
+        Attribute(const string unit, const string desc, const string iden) : unit(unit), description(desc), identifier(iden) {
+
+        }
+
+        Attribute() {
+            unit = description = identifier = nullptr;
+        }
+
+        virtual const string getUnit() const {
+            return unit;
+        }
+
+        virtual const string getDescription() const {
+            return description;
+        }
+
+        virtual const string getIdentifier() const {
+            return identifier;
+        }
+        
+    private:
+        string unit;
+        string description;
+        string identifier;
+
+};
